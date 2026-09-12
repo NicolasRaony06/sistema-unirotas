85522feddd43ef2d82ba0f4ac9530ecee1804b63
keven lohan bezerra neves <lohankeven983@outlook.com>
2026-09-12 13:50:41 -0300

    feature --> telas de esqueci senha personalizada seguindo o design proposto no figma

--- authentication/urls.py
+++ authentication/urls.py
@@ -29,14 +29,20 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
 
     path('password_reset/', auth_views.PasswordResetView.as_view(
+        template_name='registration/password_reset_form.html',
         success_url=reverse_lazy('authentication:password_reset_done')
     ), name='password_reset'),
-    
-    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
-    
+
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(
+        template_name='registration/password_reset_done.html'
+    ), name='password_reset_done'),
+
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
+        template_name='registration/password_reset_confirm.html',
         success_url=reverse_lazy('authentication:password_reset_complete')
     ), name='password_reset_confirm'),
-    
-    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(
+        template_name='registration/password_reset_complete.html'
+    ), name='password_reset_complete'),
 ]
