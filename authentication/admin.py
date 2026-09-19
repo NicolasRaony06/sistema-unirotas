006422e969762593ce9ab54b43621f5393a20889
Álvaro  Silva <alvaro.chaves@escolar.ifrn.edu.br>
2026-09-19 15:41:50 -0300

    feat(admin): registrando modelos no painel de administracao

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import *
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    ...
 
+@admin.register(StudentProfile)
+class StudentAdmin(admin.ModelAdmin):
+    ...
 # Register your models here.
