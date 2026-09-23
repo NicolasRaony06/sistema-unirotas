e9c941ffdcb3834c396a98e9bac21c963a775617
Álvaro  Silva <alvaro.chaves@escolar.ifrn.edu.br>
2026-09-23 10:48:11 -0300

    Update(urls): Criando namespace app_name dentro da url(Sera util no redirect em authentication), e criando a url que levara para a home inicial

--- management/urls.py
+++ management/urls.py
@@ -1,6 +1,9 @@
 from .views import *
 from django.urls import path
 
+app_name = 'management'
+
 urlpatterns = [
-    path('cadastro/', municipio),
+    path('home/',home, name='home'),
+    path('cadastrar-municipio/',criar_municipio,name='criar-municipio '),
 ]
