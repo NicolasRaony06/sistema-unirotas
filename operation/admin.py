80c32d66a86343f4c8405cc092b1f7e66c63779a
Janduijr <jandui500@gmail.com>
2026-09-23 15:18:32 -0300

    feature --> criando viagem e usuario

--- operation/admin.py
+++ operation/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from operation.models import Viagem
+from operation.models import Viagem, UsuarioDaViagem
 
 # Register your models here.
 
 admin.site.register(Viagem)
+admin.site.register(UsuarioDaViagem)
 
--- operation/models.py
+++ operation/models.py
@@ -38,5 +38,15 @@ class Viagem(models.Model):
         return None
     
 class UsuarioDaViagem(models.Model):
-    estudante = models.CharField(max_length=50)
-    Viagem
+    estudante = models.CharField(max_length=50, unique=True)
+    viagem = models.ForeignKey(Viagem, on_delete=models.CASCADE, unique=True)
+    direcao = models.CharField(max_length=20, choices=[
+        ('ida', 'Ida'),
+        ('volta', 'Volta'),
+        ('ida e volta', 'Ida e Volta'),
+    ])
+    presente = models.BooleanField(default=False)
+    instituicao = models.CharField(max_length=60)
+    
+    def __str__(self):
+        return self.estudante
