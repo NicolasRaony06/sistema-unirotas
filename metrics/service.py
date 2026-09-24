85d718558f5a9ef9441c0e51e8ca6a267a1f8254
keven lohan bezerra neves <lohankeven983@outlook.com>
2026-09-24 17:13:22 -0300

    feature --> agora tem um service para calculo de todas paradas de uma vez

--- metrics/service.py
+++ metrics/service.py
@@ -5,6 +5,7 @@ from django.db import transaction
 from django.utils import timezone
 from django.utils.dateparse import parse_datetime
 from decimal import Decimal
+from datetime import timedelta
 
 def register_student(user, route):
     StudentsUsingBus.objects.create(user=user, route=route)
@@ -85,3 +86,65 @@ def get_time_prediction(line, route, order, distance):
     current_distance = convert_km_to_m(distance) #preciso pegar a distancia da ordem atual da tabela RotaParadas
     time = Decimal(str(current_distance))/Decimal(str(velocity))
     return math.ceil(time)
+
+def start_trip_metric(line, route, start_stop, ttl=28800):
+    now = timezone.now()
+    today = now.date()
+
+    cache_key = f"trip_start:{line}:{route}:{today}"
+
+    cache.set(
+        cache_key,
+        {
+            "start_stop": start_stop,
+            "start_time": now,
+        },
+        timeout=ttl,
+    )
+
+
+#não da para testar ainda
+def build_route_timeline_prediction(line, route, current_order: int = 1, base_time=None):
+    if base_time is None:
+        base_time = timezone.now()
+
+    route_stops = RouteStop.objects.filter(route=route).select_related('start_stop', 'end_stop').order_by('order')
+    
+    timeline = []
+    accumulated_seconds = 0
+
+    for rs in route_stops:
+        if rs.order <= current_order:
+            status = "concluida" if rs.order < current_order else "atual"
+            step_seconds = 0
+            eta = None  # Ou o horário real em que passou, se tiver registrado
+        else:
+            status = "pendente"
+            # Predição do trecho usando a velocidade da etapa (rs.order - 1)
+            step_seconds = get_time_prediction(
+                line=line,
+                route=route,
+                current_order=rs.order,
+                distance_km=rs.distance
+            )
+            accumulated_seconds += step_seconds
+
+            eta = base_time + timedelta(seconds=accumulated_seconds)
+
+        timeline.append({
+            'order': rs.order,
+            'start_stop': rs.start_stop.name,
+            'end_stop': rs.end_stop.name,
+            'status': status,
+            'distance_km': rs.distance,
+            'step_seconds': step_seconds,
+            'accumulated_seconds': accumulated_seconds,
+            'eta': eta,  # Objeto datetime nativo
+        })
+
+    return {
+        'current_order': current_order,
+        'base_time': base_time,
+        'total_remaining_seconds': accumulated_seconds,
+        'timeline': timeline
+    }
