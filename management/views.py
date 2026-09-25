09200e7dd270c65ecddf615ade03c32bbdc24f43
NicolasRaony06 <nicolas.raony06@gmail.com>
2026-09-25 15:01:32 -0300

    Add: validações de login e role

--- management/views.py
+++ management/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.urls import reverse_lazy
 from datetime import datetime
 from django.http import HttpResponse, JsonResponse
 from .models import *
 from authentication.models import UserRole, PersonelProfile
 from authentication.service import generate_elevated_signup_link
+from authentication.decorators import role_required
 from .forms import BusForm
 from .handlers import cadastrar_municipio
 # Create your views here.
@@ -32,7 +35,8 @@ def criar_municipio(request):
     
     return HttpResponse(f"Municipio criado com sucesso por: {request.user.role}")
 
-#TODO adicionar login e keven decorador
+@login_required(login_url=reverse_lazy('authentication:login'))
+@role_required(allowed_roles=UserRole.MANAGER)
 def invite_driver(request):
     #TODO alterar por decorator de keven
     if request.user.role != UserRole.MANAGER:
@@ -62,7 +66,8 @@ def invite_driver(request):
 
     return render(request, "invite_driver.html")
 
-#TODO adicionar login e keven decorador
+@login_required(login_url=reverse_lazy('authentication:login'))
+@role_required(allowed_roles=UserRole.MANAGER)
 def register_bus(request):
     if request.user.role != UserRole.MANAGER:
         messages.error(request, "Você precisa estar logado como um gestor para ter acesso.")
@@ -75,7 +80,7 @@ def register_bus(request):
             bus.city = request.user.personel_profile.city
             bus.save()
             return redirect('management:home')
-        
+
         messages.error(request, "Ocorreu um erro ao tentar cadastrar o ônibus. Tente novamente.")
     else:
         form = BusForm()
